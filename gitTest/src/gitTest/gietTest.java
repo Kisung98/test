aa671065da87e8991115441c69d8937b0a38c653
Kisung98 <godbuyer1@naver.com>
2024-01-07 14:09:30 +0900

    처음 프로젝트를 올렸음

---
+++ gitTest/src/gitTest/gietTest.java
@@ -0,0 +1,21 @@
+package gitTest;
+
+public class gietTest
+{
+
+	public static void main(String[] args)
+	{
+		int a = 5;
+
+		if (true)
+		{
+			System.out.println("5보다 큽니다.");
+		}
+		if (a == 5)
+		{
+			System.out.println("요기있네");
+		}
+
+	}
+
+}
